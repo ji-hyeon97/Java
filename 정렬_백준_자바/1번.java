c5320d0e48e2b2feed3b294e2422dd0a8694b16a
ji-hyeon97 <79920930+ji-hyeon97@users.noreply.github.com>
2021-04-29 21:38:18 +0900

    Add files via upload

---
+++ 정렬_백준_자바/1번.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner user = new Scanner(System.in);
+        int n = user.nextInt();
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = user.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (array[i] < array[j]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        /*
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+         */
+        for (int i = 0; i < n; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
